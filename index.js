3bd97ce46774250f99d7493f795ee9bf9f83208d
Richard <raikoroje@smarttech.com>
2015-09-24 17:17:00 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/* jshint node: true */
+'use strict';
+
+var derequire = require('broccoli-derequire');
+
+module.exports = {
+  name: 'ember-derequire',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    this.options = app.options.derequire || {};
+
+    var defaultOptions = {enabled: true};
+    
+    for (var option in defaultOptions) {
+      if (!this.options.hasOwnProperty(option)) {
+        this.options[option] = defaultOptions[option];
+      }
+    }
+  },
+
+  postprocessTree: function(type, tree) {
+    if (type === 'all' && this.options && this.options.enabled) {
+      tree = derequire(tree, this.options);
+    }
+
+    return tree;
+  }
+};
