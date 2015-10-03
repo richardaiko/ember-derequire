b352849fed2f23ebc698913a67638e8bfa21c651
Richard Aikoroje <raikoroje@smarttech.com>
2015-10-03 00:09:37 -0600

    Disable derequire in test environments by default

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = {
     this._super.included.apply(this, arguments);
     this.options = app.options.derequire || {};
 
-    var defaultOptions = {enabled: true};
+    var defaultOptions = {enabled: this.app.env !== 'test'};
     
     for (var option in defaultOptions) {
       if (!this.options.hasOwnProperty(option)) {
@@ -20,16 +20,19 @@ module.exports = {
   },
 
   postprocessTree: function(type, tree) {
-    if (type === 'all' && this.options && this.options.enabled) {
+    if (type === 'all' && this.isEnabled()) {
       tree = derequire(tree, this.options);
     }
-
     return tree;
   },
   
   contentFor:function (type) {
-    if(type === 'app-boot'){
+    if(type === 'app-boot' && this.isEnabled()){
       return 'var define = define; var require = require;'
     }
+  },
+  
+  isEnabled:function() {
+    return this.options && this.options.enabled;
   }
 };
